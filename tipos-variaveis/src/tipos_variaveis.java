2fda6c4f484ed5fab92976ac516e7935793b208b
stchanalytics <sarah.agc125@gmail.com>
2024-05-31 07:30:23 -0300

    Concatenação de texto e operadores lógicos

--- tipos-variaveis/src/tipos_variaveis.java
+++ tipos-variaveis/src/tipos_variaveis.java
@@ -1,5 +1,38 @@
 public class tipos_variaveis {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+
+        int numero = 5;
+        
+        numero = - numero; // nega o valor da variavel principal
+       /*  System.out.println(- numero);*/ // nao afeta a variavel principal
+
+        System.out.println(numero);
+
+        numero = numero * -1; // torna o valor positivo novamente
+
+        System.out.println(numero);
+
+        
     }
 }
+/*String nomeCompleto= "Linguagem" + "Java"; // concatenação de texto 
+        System.out.println(nomeCompleto);
+
+        String concatenacao = "?";
+        System.out.println(concatenacao);
+
+        concatenacao = 1+1+1+"1";
+        System.out.println(concatenacao);
+
+        concatenacao = 1+"1"+1+1;
+        System.out.println(concatenacao);
+
+        concatenacao = 1+"1"+1+"1";
+        System.out.println(concatenacao);
+
+        concatenacao = 1+1+1+"P";
+        System.out.println(concatenacao);
+
+        concatenacao = "K"+(1+1+2);
+        System.out.println(concatenacao);
+        */
