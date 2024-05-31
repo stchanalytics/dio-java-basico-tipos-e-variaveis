93bce9fb05211ac835a52830da2163602bcfc973
stchanalytics <sarah.agc125@gmail.com>
2024-05-31 08:24:19 -0300

    Operador Ternário

--- tipos-variaveis/src/tipos_variaveis.java
+++ tipos-variaveis/src/tipos_variaveis.java
@@ -1,30 +1,17 @@
 public class tipos_variaveis {
     public static void main(String[] args) {
+        int a,b;
 
-        int numero = 5;
-        
-       //x repeticao
-       numero ++;
-       //numero = numero        
+        a=5;
+        b=6;
+        //operador ternário
+        // pode utilizar variaveis do tipo int
+        String resultado = a==b ? "verdadeiro" : "falso"; //sempre sera booleana
 
-       System.out.println(numero);
-
-// aplica-se a mesma logica para decremento (--)
-       System.out.println(numero ++); // caso queira que o incremento seja após a impressão
-        System.out.println(numero);
-        System.out.println(++ numero); // imprime o incremento no primeiro print
-
-        boolean variavel = true;
-
-        System.out.println(!variavel); // altera apenas na memoria, nao é definitiva
-        System.out.println(variavel);
-
-        variavel = !variavel; // negação de boolean
-
-        System.out.println(variavel);
-
+        System.out.println(resultado);
     }
 }
+
 /*String nomeCompleto= "Linguagem" + "Java"; // concatenação de texto 
         System.out.println(nomeCompleto);
 
@@ -57,4 +44,26 @@ public class tipos_variaveis {
         numero = numero * -1; torna o valor positivo novamente
 
         System.out.println(numero); */
+//---------------------------------------------------------------------
+        /*  int numero = 5;
+        
+       //x repeticao
+       numero ++;
+       //numero = numero        
 
+       System.out.println(numero);
+
+// aplica-se a mesma logica para decremento (--)
+       System.out.println(numero ++); // caso queira que o incremento seja após a impressão
+        System.out.println(numero);
+        System.out.println(++ numero); // imprime o incremento no primeiro print
+
+        boolean variavel = true;
+
+        System.out.println(!variavel); // altera apenas na memoria, nao é definitiva
+        System.out.println(variavel);
+
+        variavel = !variavel; // negação de boolean
+
+        System.out.println(variavel); */
+// ---------------------------------------------------------------------
